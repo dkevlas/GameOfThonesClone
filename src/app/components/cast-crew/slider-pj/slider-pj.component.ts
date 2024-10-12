2e426b2c4ad8bdd1dff57c1c98944d793b9029e8
Kevin <dennisk.blas@gmail.com>
2024-10-12 16:28:59 -0500

    Estructura, estilos y logica para la seleccion de personaje y muestra de informacion

---
+++ src/app/components/cast-crew/slider-pj/slider-pj.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Character, characters } from '../characters';
+
+@Component({
+  selector: 'app-slider-pj',
+  templateUrl: './slider-pj.component.html',
+  styleUrl: './slider-pj.component.scss'
+})
+export class SliderPjComponent implements OnInit{
+
+  charactersItems: Character[] = characters
+
+  cantCharacters: number = this.charactersItems.length
+  widthImg: number = 75
+  left: string = '0px'
+  leftCurrent: number = parseInt(this.left)
+
+  prevDisabled: boolean = false
+  nextDisabled: boolean = true
+
+  btnPrev(){
+    this.nextDisabled = false
+    this.leftCurrent -= this.widthImg
+    this.left = `${this.leftCurrent}px`
+    if(this.left == `${-(this.cantCharacters * this.widthImg) + 300}px`){
+      this.prevDisabled = true
+    }
+  }
+
+  btnNext(){
+    this.prevDisabled = false
+    this.leftCurrent += this.widthImg
+    this.left = `${this.leftCurrent}px`
+    if(this.left == '0px'){
+      this.nextDisabled = true
+    }
+  }
+
+  selectIndex: number = 1
+
+  title: string = ''
+  paragraph: string = ''
+  bgImage: string = ''
+
+  changeSelect(n: number){
+    this.selectIndex = n
+    this.title = this.charactersItems[this.selectIndex].name
+    this.paragraph = this.charactersItems[this.selectIndex].description
+    this.bgImage = this.charactersItems[this.selectIndex].bgCharacter
+  }
+  
+  ngOnInit(): void {
+    this.changeSelect(this.selectIndex)
+  }
+}
---
+++ src/app/components/cast-crew/slider-pj/summary-pj/summary-pj.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-pj',
+  templateUrl: './summary-pj.component.html',
+  styleUrl: './summary-pj.component.scss'
+})
+export class SummaryPjComponent {
+  @Input()
+  title: string = ''
+
+  @Input()
+  paragraph: string = ''
+
+  @Input()
+  bgImage: string = ''
+}
