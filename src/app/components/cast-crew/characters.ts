91bd1710ce6a39a5d4b38bfe128ee280fe46fc4d
Kevin <dennisk.blas@gmail.com>
2024-10-11 21:03:16 -0500

    Informacion para intercambiar con la lógica TS, para seleccionara al personaje

---
+++ src/app/components/cast-crew/characters.ts
@@ -0,0 +1,80 @@
+export interface Character{
+    img: string,
+    name: string,
+    description: string,
+    bgCharacter: string,
+    alt: string
+}
+
+export const characters: Character[] = [
+    {
+        img: 'https://www.grazia.fr/wp-content/uploads/grazia/2023/08/sydney-sweeney-2.jpg',
+        name: 'Sydney Sweeney',
+        description: 'Tetona Senxual',
+        bgCharacter: '',
+        alt: ''
+    },
+    {
+        img: 'https://variety.com/wp-content/uploads/2021/07/Taylor-Swift.jpg?w=1000&h=667&crop=1',
+        name: 'Taylor Swift',
+        description: 'Hermosa Quinceañera',
+        bgCharacter: '',
+        alt: ''
+    },
+    {
+        img: 'https://media-cdn-v2.laodong.vn/storage/newsportal/2024/7/11/1364829/Lisa-Blackpink-1A11.jpg?w=800&h=420&crop=auto&scale=both',
+        name: 'Lisa Manoban',
+        description: 'Flaquita loka',
+        bgCharacter: '',
+        alt: ''
+    },
+    {
+        img: 'https://i.ebayimg.com/images/g/-K8AAOSwSglkCOKI/s-l1200.jpg',
+        name: 'Emma Myers',
+        description: 'Linda bella',
+        bgCharacter: '',
+        alt: ''
+    },
+    {
+        img: 'https://img.20mn.fr/nyTyhgVdRLKe-X42wGw8vA/1444x920_actrice-scarlett-johansson',
+        name: 'Scarlett Johansson',
+        description: 'Viuda Hermosa',
+        bgCharacter: '',
+        alt: ''
+    },
+    {
+        img: 'https://bilder.fernsehserien.de/gfx/person_1000/m/milla-jovovich-181041-1647704280-w-893.jpg.jpg',
+        name: 'Milla Jovovich',
+        description: 'Ojos hermosos',
+        bgCharacter: '',
+        alt: ''
+    },
+    // {
+    //     img: 'https://www.viapais.com.ar/resizer/v2/ZOAIZDWD5FFABDD5INAABDC6IA.webp?quality=75&smart=true&auth=b678fe1ab5ce01ba8db3cccc34cbaa309aff11dec7f105d4375157e0d10d7d9c&width=980&height=640',
+    //     name: 'Lisa',
+    //     description: '',
+    //     bgCharacter: '',
+    //     alt: ''
+    // },
+    // {
+    //     img: 'https://media.revistavanityfair.es/photos/60e84eb2f65f37760f5bf890/master/w_1600%2Cc_limit/28068.jpg',
+    //     name: 'Lisa',
+    //     description: '',
+    //     bgCharacter: '',
+    //     alt: ''
+    // },
+    // {
+    //     img: 'https://elcomercio.pe/resizer/v2/AJQAFALYSVEZLNYUDA455IYQXE.jpg?auth=c17bf37310c9e0c23a39df6d34652d50b7d2c1af725f8119ae44ec484cb4ef6e&width=1200&height=810&quality=90&smart=true',
+    //     name: 'Lisa',
+    //     description: '',
+    //     bgCharacter: '',
+    //     alt: ''
+    // },
+    // {
+    //     img: 'https://lh3.googleusercontent.com/BKgJ2Qcko6lgLsnvB-URUnK2bgQ682OoJd7Dpa0829XrmIC40zfsFm74vNdmyaMBx8xUt-MHFwDIUUC9=w544-h544-l90-rj',
+    //     name: 'Lisa',
+    //     description: '',
+    //     bgCharacter: '',
+    //     alt: ''
+    // },
+]
