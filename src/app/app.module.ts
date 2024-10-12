1736a2b2152c8c82372d3ad840f171cc4e770561
Kevin <dennisk.blas@gmail.com>
2024-10-11 20:58:51 -0500

    Base para la configuracion de estilos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SliderComponent } from './components/slider/slider.component';
+import { CastCrewComponent } from './components/cast-crew/cast-crew.component';
+import { SliderPjComponent } from './components/cast-crew/slider-pj/slider-pj.component';
+import { SummaryPjComponent } from './components/cast-crew/slider-pj/summary-pj/summary-pj.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SliderComponent
+    CastCrewComponent,
+    SliderPjComponent,
+    SummaryPjComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule,
