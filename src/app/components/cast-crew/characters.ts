b9b64a37d36aa2f20747a355dde2c5c3469fb9c4
Kevin <dennisk.blas@gmail.com>
2024-10-13 17:10:49 -0500

    Actualizacion de Informacion para los personajes con sus respectivos enlaces S3 AWS

--- src/app/components/cast-crew/characters.ts
+++ src/app/components/cast-crew/characters.ts
@@ -8,72 +8,107 @@ export interface Character{
 
 export const characters: Character[] = [
     {
-        img: 'https://www.grazia.fr/wp-content/uploads/grazia/2023/08/sydney-sweeney-2.jpg',
-        name: 'Sydney Sweeney',
-        description: 'Tetona Senxual',
-        bgCharacter: '',
-        alt: ''
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Jaime-Lannister-Profile.png',
+        name: 'Jaime Lannister',
+        description: 'Conocido como el "Matarreyes" por asesinar al rey al que servía, Jaime comienza como un hombre arrogante. Sin embargo, su redención y su relación con Brienne de Tarth lo muestran como un hombre más complejo y honorable.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Jaime-Lannister.png',
+        alt: 'Jaime Lannister'
     },
     {
-        img: 'https://variety.com/wp-content/uploads/2021/07/Taylor-Swift.jpg?w=1000&h=667&crop=1',
-        name: 'Taylor Swift',
-        description: 'Hermosa Quinceañera',
-        bgCharacter: '',
-        alt: ''
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Tyrion-Lannister-Profile.png',
+        name: 'Tyrion Lannister',
+        description: 'El inteligente y sarcástico enano de la Casa Lannister, Tyrion es subestimado por su familia. A lo largo de la serie, demuestra ser uno de los estrategas más brillantes, navegando la política y las intrigas de Westeros.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Tyrion-Lannister.png',
+        alt: 'Tyrion Lannister'
     },
     {
-        img: 'https://media-cdn-v2.laodong.vn/storage/newsportal/2024/7/11/1364829/Lisa-Blackpink-1A11.jpg?w=800&h=420&crop=auto&scale=both',
-        name: 'Lisa Manoban',
-        description: 'Flaquita loka',
-        bgCharacter: '',
-        alt: ''
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Cersei-Lannister-Profile.png',
+        name: 'Cersei Lannister',
+        description: 'Ambiciosa y despiadada, Cersei busca consolidar el poder de los Lannister y proteger a sus hijos a toda costa. A lo largo de la serie, se enfrenta a sus enemigos con crueldad y astucia, pero su obsesión con el poder la lleva a su caída.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Cersei-Lannister.png',
+        alt: 'Cersei Lannister'
     },
     {
-        img: 'https://i.ebayimg.com/images/g/-K8AAOSwSglkCOKI/s-l1200.jpg',
-        name: 'Emma Myers',
-        description: 'Linda bella',
-        bgCharacter: '',
-        alt: ''
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Daenerys-Targaryen-Profile.png',
+        name: 'Daenerys Targaryen',
+        description: 'La última de la Casa Targaryen, Daenerys pasa de ser una joven oprimida a una poderosa reina, conocida por liberar esclavos y gobernar con dragones. Su lucha por recuperar el Trono de Hierro la transforma a lo largo de la serie.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Daenerys-Targaryen.png',
+        alt: 'Daenerys Targaryen'
     },
     {
-        img: 'https://img.20mn.fr/nyTyhgVdRLKe-X42wGw8vA/1444x920_actrice-scarlett-johansson',
-        name: 'Scarlett Johansson',
-        description: 'Viuda Hermosa',
-        bgCharacter: '',
-        alt: ''
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Jon-Snow-Profile.png',
+        name: 'Jon Snow',
+        description: 'Hijo bastardo de Eddard Stark, Jon comienza su historia en la Guardia de la Noche, buscando su lugar en el mundo. A lo largo de la serie, descubre secretos sobre su verdadero linaje y se convierte en un líder clave en la lucha contra el mal.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Jon-Snow.png',
+        alt: 'Jon Snow'
     },
     {
-        img: 'https://bilder.fernsehserien.de/gfx/person_1000/m/milla-jovovich-181041-1647704280-w-893.jpg.jpg',
-        name: 'Milla Jovovich',
-        description: 'Ojos hermosos',
-        bgCharacter: '',
-        alt: ''
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Sansa-Stark-Profile.png',
+        name: 'Sansa Stark',
+        description: 'Al principio una joven ingenua y soñadora, Sansa sufre traiciones y abusos, pero aprende a jugar el juego político. Eventualmente, emerge como una líder astuta y la clave en la reconstrucción del Norte.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Sansa-Stark.png',
+        alt: 'Sansa Stark'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Khal-Drogo-Profile.png',
+        name: 'Khal Drogo',
+        description: 'Líder de los Dothraki y esposo de Daenerys, Khal Drogo es un guerrero brutal y temido. Aunque comienza como un personaje imponente, su amor por Daenerys muestra su lado más suave, antes de su trágica muerte.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Khal-Drogo.png',
+        alt: 'Khal Drogo'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Bran-Stark-Profile.png',
+        name: 'Bran Stark',
+        description: 'Al principio de la serie, Bran es un niño aventurero que queda paralizado tras un accidente. A medida que descubre sus habilidades como el Cuervo de Tres Ojos, su rol evoluciona hacia el de una figura mística con poderes sobre el tiempo y la memoria.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Bran-Stark.png',
+        alt: 'Bran Stark'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Arya-Stark-Profile.png',
+        name: 'Arya Stark',
+        description: 'La hija menor de los Stark, Arya rechaza las normas tradicionales para las mujeres nobles y se embarca en un viaje de venganza y autodescubrimiento. Entrenada como una asesina mortal, se convierte en una fuerza imparable.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Arya-Stark.png',
+        alt: 'Arya Stark'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Eddard-Stark-Profile.png',
+        name: 'Eddard (Ned) Stark',
+        description: 'El honorable líder de la Casa Stark y Guardián del Norte, Ned se rige por su sentido del deber y la justicia. Su incapacidad para jugar el juego de poder en Desembarco del Rey lo lleva a su trágica muerte, que desencadena los eventos posteriores de la serie.',
+        bgCharacter: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/cast-and-crew/Eddard-Stark.png',
+        alt: 'Eddard (Ned) Stark'
     },
     // {
-    //     img: 'https://www.viapais.com.ar/resizer/v2/ZOAIZDWD5FFABDD5INAABDC6IA.webp?quality=75&smart=true&auth=b678fe1ab5ce01ba8db3cccc34cbaa309aff11dec7f105d4375157e0d10d7d9c&width=980&height=640',
-    //     name: 'Lisa',
-    //     description: '',
+    //     img: '',
+    //     name: 'Theon Greyjoy',
+    //     description: 'Criado como pupilo de los Stark, Theon lucha con su identidad entre los Stark y su propia familia Greyjoy. Después de traicionar a los Stark, su arco gira en torno a su doloroso viaje hacia la redención.',
     //     bgCharacter: '',
     //     alt: ''
     // },
     // {
-    //     img: 'https://media.revistavanityfair.es/photos/60e84eb2f65f37760f5bf890/master/w_1600%2Cc_limit/28068.jpg',
-    //     name: 'Lisa',
-    //     description: '',
+    //     img: '',
+    //     name: 'Jorah Mormont',
+    //     description: 'Exiliado de Westeros, Jorah se convierte en el consejero más leal de Daenerys. Aunque está enamorado de ella, su devoción a su causa lo define, incluso en su sacrificio final.',
     //     bgCharacter: '',
     //     alt: ''
     // },
     // {
-    //     img: 'https://elcomercio.pe/resizer/v2/AJQAFALYSVEZLNYUDA455IYQXE.jpg?auth=c17bf37310c9e0c23a39df6d34652d50b7d2c1af725f8119ae44ec484cb4ef6e&width=1200&height=810&quality=90&smart=true',
-    //     name: 'Lisa',
-    //     description: '',
+    //     img: '',
+    //     name: 'Sandor Clegane (El Perro)',
+    //     description: 'Un guerrero brutal con una cicatriz física y emocional, Sandor es conocido por su desprecio por la caballería y la moralidad. A lo largo de la serie, su desarrollo lo convierte en un antihéroe con un código moral propio.',
     //     bgCharacter: '',
     //     alt: ''
     // },
     // {
-    //     img: 'https://lh3.googleusercontent.com/BKgJ2Qcko6lgLsnvB-URUnK2bgQ682OoJd7Dpa0829XrmIC40zfsFm74vNdmyaMBx8xUt-MHFwDIUUC9=w544-h544-l90-rj',
-    //     name: 'Lisa',
-    //     description: '',
+    //     img: '',
+    //     name: ' Brienne de Tarth',
+    //     description: 'Fuerte, honorable y leal, Brienne es una de las pocas mujeres caballeras de Westeros. Su devoción por proteger a los Stark y su relación con Jaime Lannister la convierten en un personaje fundamental en la serie.',
+    //     bgCharacter: '',
+    //     alt: ''
+    // },
+    // {
+    //     img: '',
+    //     name: ' Samwell Tarly',
+    //     description: 'Miembro de la Guardia de la Noche y mejor amigo de Jon Snow, Sam es un hombre tímido y con sobrepeso que se convierte en un estudioso clave para la derrota de los Caminantes Blancos, demostrando que el conocimiento es tan valioso como la espada.',
     //     bgCharacter: '',
     //     alt: ''
     // },
