e2cf086bbbd52b6f72cd4422dd3c0531dae5d7cc
Kevin <dennisk.blas@gmail.com>
2024-10-15 11:15:12 -0500

    archivo para los enlances e informacion de la galeria

---
+++ src/app/components/gallery/images.ts
@@ -0,0 +1,68 @@
+export interface itemImage {
+    img: string,
+    alt: string,
+    text: string
+}
+
+export const listImages: itemImage[] = [
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Yara-Greyjoy-la-unica-que-se-preocupa-por-su-hermano.webp',
+        alt: 'Yara Greyjoy: la única que se preocupa por su hermano',
+        text: 'Bajo la dura y espinosa fachada de Yara Greyjoy, se revela una persona cariñosa y amable, quien destaca por ser la única en su familia que realmente se preocupa por su hermano.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Daenerys-escucha-las-campanas-y-desata-su-locura-sobre-la-capital.webp',
+        alt: 'Daenerys escucha las campanas y desata su locura sobre la capital',
+        text: 'Daenerys, dividida entre su dolor y su deseo de venganza, escucha las campanas y decide desatar su furia, arrasando la capital con fuego y destrucción.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Catelyn-Stark-desespera-por-salvar-a-Robb-Stark.webp',
+        alt: 'Catelyn Stark desespera por salvar a Robb Stark',
+        text: 'Catelyn Stark vive una profunda desesperación al intentar salvar a su hijo Robb Stark. A pesar de sus súplicas y esfuerzos, no es escuchada, y su angustia culmina en la trágica muerte de ambos durante la Boda Roja'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Los-primeros-hombres-nos-llaman-Los-Hijos.webp',
+        alt: 'Los primeros hombres nos llaman: "Los hijos"',
+        text: 'Los Hijos del Bosque, unas misteriosas criaturas que habitaron Poniente antes que los hombres.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Nacimiento-de-los-dragones.webp',
+        alt: 'Nacimiento de los dragones',
+        text: 'Al morir Khal Drogo, Daenerys Targaryen quema el cuerpo de su esposo, y con él los tres huevos de dragón. A raíz de este acto, los dragones nacen.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Dario-y-Khaleesi.webp',
+        alt: 'Dario y Khaleesi',
+        text: 'Daario es uno de los pocos personajes que han acompañado a Daenerys en su viaje, ahora que sir Jorah Mormont ha caído defendiendo a la Khaleesi de los Caminantes Blancos.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Drogon-y-los-hijos-de-la-Arp%C3%ADa.webp',
+        alt: 'Drogon y los hijos de la Arpía',
+        text: 'El hijo mayor de Daenerys, Drogón, aparece para rescatarla de las manos de Los Hijos de la Arpía, quienes la tienen acorralada para asesinarla.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Daenerys-consigue-su-nuevo-ej%C3%A9rcito-de-inmaculados.webp',
+        alt: 'Daenerys consigue su nuevo ejército de inmaculados',
+        text: `Daenerys intercambia un dragón por un ejército de Inmaculados y con 'Dracarys' acaba con el esclavista.`
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Daenerys-recibe-los-huevos.webp',
+        alt: 'Daenerys-recibe-los-huevos',
+        text: 'El día de la boda entre Daenerys Targaryen y Khal Drogo, la joven recibió como regalo tres huevos de dragón de parte del Magister Illyrio Mopatis.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Yara-Greyjoy-y-los-hijos-del-hierro.webp',
+        alt: 'Yara Greyjoy y los hijos del Hierro',
+        text: 'La nueva señora de las Islas del Hierro le dijo a su hermano que volvería a su hogar para preparar todo en caso de que Daenerys necesitase un refugio de los Caminantes Blancos.'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Daenerys-Targaryen-se-dirije-a-Poniente.webp',
+        alt: 'Daenerys Targaryen se dirije a Poniente',
+        text: 'Daenerys Targaryen parece invencible pues ahora avanza hacia su destino con sus tres dragones, el ejército de Inmaculados y el de los dothraki'
+    },
+    {
+        img: 'https://global-media-repository.s3.us-east-2.amazonaws.com/clones/got/gallery/Cersei-es-humillada-en-el-paseo-de-la-verguenza.webp',
+        alt: 'Cersei es humillada en el paseo de la vergüenza',
+        text: 'Es una de las escenas más icónicas de la serie, mostrando su humillación pública al son de "shame". Este evento marca el comienzo de una venganza brutal.'
+    }
+]
