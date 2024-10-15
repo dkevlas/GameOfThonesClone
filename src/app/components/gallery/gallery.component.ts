f6291b73e22674bcefb796c12985061b099a9a67
Kevin <dennisk.blas@gmail.com>
2024-10-15 11:16:53 -0500

    estructura, estilos, lógica para la seccion de galeria

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { itemImage } from './images';
+import { listImages } from './images';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class GalleryComponent {
 
+  allItems: itemImage[] = listImages
+
+  oneItem: itemImage = this.allItems[1]
+
+  active: number = 1
+  topPosition: string = '0px'
+
+  sendInfo(i: number){
+    this.oneItem = this.allItems[i]
+  }
+
+  cantImages: number = this.allItems.length
+  
+  moveSizeHeight: number = 70
+  changePosition(indexCurrent: number){
+    this.active = indexCurrent
+    console.log(indexCurrent)
+    if(indexCurrent < 2 ){
+      this.topPosition = '0px'
+    } else if (indexCurrent > (this.cantImages - 3)){
+      this.topPosition = this.topPosition
+    } else {
+      this.topPosition = `-${this.moveSizeHeight * (indexCurrent - 1)}px`
+    }
+  }
 }
