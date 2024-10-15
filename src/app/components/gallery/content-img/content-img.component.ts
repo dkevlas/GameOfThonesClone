6468d3e5ec092639bfd95c540a9f3ba52f831b6d
Kevin <dennisk.blas@gmail.com>
2024-10-15 11:17:50 -0500

    para el componente child de Galeria se completó la estructura, estilos y lógica

---
+++ src/app/components/gallery/content-img/content-img.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { itemImage } from '../images';
+
+@Component({
+  selector: 'app-content-img',
+  templateUrl: './content-img.component.html',
+  styleUrl: './content-img.component.scss'
+})
+export class ContentImgComponent implements OnInit {
+
+  @Input()
+  itemShow!: itemImage
+
+  img: string = ''
+  ngOnInit(): void {
+    this.img = this.itemShow.img
+      this.itemShow
+      console.log(this.img)
+  }
+}
